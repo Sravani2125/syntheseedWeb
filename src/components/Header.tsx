ea62c560b7ccdee7d0fe1e45df39f40139bf00c9
Sravani <sravanisingam2@gmail.com>
2025-10-22 18:18:15 +0530

    fixed build issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Moon, Sun, Menu, X } from 'lucide-react';
 import { useTheme } from '../contexts/ThemeContext';
-import logo from '../../../project/assets/logo.png';
+import logo from '../../assets/logo.png';
 const Header = () => {
   const { theme, toggleTheme } = useTheme();
   const [isScrolled, setIsScrolled] = useState(false);
